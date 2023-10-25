a1962c893fb6e634d8bb6b42031101b3bb9d7eae
JesúsSente <jesussente@.com>
2023-10-25 16:33:14 -0600

    navbar y vista modelo de transporte

---
+++ resources/views/modelo-transporte.blade.php
@@ -0,0 +1,12 @@
+@extends('welcome')
+
+@section('content')
+
+<link rel="stylesheet" type="text/css" href="{{ asset('css/estilos.css') }}">
+<script src="{{ asset('js/app.js') }}"></script>
+
+<div>
+    <h1>hola mundo</h1>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/modelo-transporte', function () {
+    return view('modelo-transporte');
+})->name('modelo');
