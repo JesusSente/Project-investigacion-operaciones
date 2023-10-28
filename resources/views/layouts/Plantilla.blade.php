7f6abb2ec764191de6a80f85ba6811751b04284d
HRDC12 <huver.donis@gmail.com>
2023-10-27 20:34:11 -0600

    Commit para entregar el proyecto

---
+++ resources/views/layouts/Plantilla.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto Final</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body>
+    <header>
+
+        
+    </header>
+    
+    <main>
+        @yield("main")
+    </main>
+    
+    <footer>
+        @yield("footer")
+    </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
